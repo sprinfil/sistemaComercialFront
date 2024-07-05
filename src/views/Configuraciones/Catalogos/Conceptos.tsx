2c1f4a41553359f83665b931a5bb0a6861edfe2d
Josue Diaz <josuediazx@outlook.com>
2024-07-05 08:54:28 -0700

    aqui meti lo del edit
    a ver si funciona

--- src/views/Configuraciones/Catalogos/Conceptos.tsx
+++ src/views/Configuraciones/Catalogos/Conceptos.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import { ContextProvider } from "../../../contexts/ContextConcepto.tsx";
 import ConceptoTable from '../../../components/Tables/Components/ConceptoTable'
 import ConceptoForm from '../../../components/Forms/ConceptoForm'
+import { useStateContext } from "../../../contexts/ContextConcepto.tsx";
 
 const Conceptos = () => {
+  
+  const { accion } = useStateContext();
+
   return (
     <ContextProvider>
       <div className='w-full max-h-[75vh] '>
@@ -15,15 +19,28 @@ const Conceptos = () => {
             <ConceptoTable />
           </div>
 
-          {/*Formulario*/}
-          <div className='w-[65%] rounded-md border border-border h-[75vh] p-4 overflow-auto'>
-            <ConceptoForm />
-          </div>
-
+          {/*Formulario edit, */}
+          <ConceptoFormEdit/>
         </div>
       </div>
     </ContextProvider>
   )
 }
+
+const ConceptoFormEdit = () => {
+
+  const { accion } = useStateContext();
+
+  return (
+    <>
+        {/*AQUI SE MANDA A LLAMAR EL FORMULARIO PERO CON LA VALIDACION SI ES EDITAR SE CAMBIE DE COLOR a*/}
+      {accion == "editar" ? (<div className='w-[65%] rounded-md border border-primary h-[75vh] p-4 overflow-auto'>
+            <ConceptoForm />
+          </div>) : (<div className='w-[65%] rounded-md border border-border h-[75vh] p-4 overflow-auto'>
+            <ConceptoForm />
+          </div>)}
+    </>
+  );
+};
 
 export default Conceptos
