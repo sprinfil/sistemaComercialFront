4a00d258fea391d5ac16a7288a9e737bb41b7d44
Cris <76029941+Kurisu-Cris@users.noreply.github.com>
2024-07-08 08:48:54 -0700

    DETALLES VISUALES

--- src/components/Forms/CrearUsuarioForm.tsx
+++ src/components/Forms/CrearUsuarioForm.tsx
@@ -81,120 +81,128 @@ const CrearUsuarioForm = () => {
                 {errors.general && <Error errors={errors.general} />}
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                        <FormField
-                            control={form.control}
-                            name="nombre"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Nombre</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Escribe el nombre del usuario" {...field} />
-                                    </FormControl>
-                                    <FormDescription>
-                                        El nombre de la crear usuario.
-                                    </FormDescription>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="apellido_paterno"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Apellido paterno</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Escribe el apellido paterno" {...field} />
-                                    </FormControl>
-                                    <FormDescription>
-                                        El apellido paterno.
-                                    </FormDescription>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="apellido_materno"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Apellido materno</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Escribe el apellido materno" {...field} />
-                                    </FormControl>
-                                    <FormDescription>
-                                        El apellido materno.
-                                    </FormDescription>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                         <FormField
-                            control={form.control}
-                            name="telefono"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Telefono</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Escribe telefono del usuario" {...field} type='number' />
-                                    </FormControl>
-                                    <FormDescription>
-                                        El telefono del usuario.
-                                    </FormDescription>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="curp"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>CURP</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Escribe la CURP" {...field} />
-                                    </FormControl>
-                                    <FormDescription>
-                                        La CURP del usuario.
-                                    </FormDescription>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="rfc"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>RFC</FormLabel>
-                                    <FormControl>
-                                        <Input  placeholder="Escribe la RFC" {...field} />
-                                    </FormControl>
-                                    <FormDescription>
-                                        El RFC del usuario.
-                                    </FormDescription>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="correo"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Correo electronico</FormLabel>
-                                    <FormControl>
-                                        <Input  placeholder="Escribe el correo electronico" {...field} type='email'/>
-                                    </FormControl>
-                                    <FormDescription>
-                                        El correo electronico del usuario.
-                                    </FormDescription>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        {loading && <Loader />}
-                        <Button type="submit">Guardar</Button>
+                        <div className="py-[40px] px-[10px] flex gap-2 w-full mb-5 rounded-md border border-border  relative">
+                            <div className="w-[50%]">
+                                <FormField
+                                    control={form.control}
+                                    name="nombre"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Nombre</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="Escribe el nombre del usuario" {...field} />
+                                            </FormControl>
+                                            <FormDescription>
+                                                
+                                            </FormDescription>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="apellido_paterno"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Apellido paterno</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="Escribe el apellido paterno" {...field} />
+                                            </FormControl>
+                                            <FormDescription>
+                                                
+                                            </FormDescription>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="apellido_materno"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Apellido materno</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="Escribe el apellido materno" {...field} />
+                                            </FormControl>
+                                            <FormDescription>
+                                               
+                                            </FormDescription>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="telefono"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Telefono</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="Escribe telefono del usuario" {...field} type='number' />
+                                            </FormControl>
+                                            <FormDescription>
+                                                
+                                            </FormDescription>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                            <div className="w-[50%]">
+                                <FormField
+                                    control={form.control}
+                                    name="curp"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>CURP</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="Escribe la CURP" {...field} />
+                                            </FormControl>
+                                            <FormDescription>
+                                                
+                                            </FormDescription>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="rfc"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>RFC</FormLabel>
+                                            <FormControl>
+                                                <Input  placeholder="Escribe la RFC" {...field} />
+                                            </FormControl>
+                                            <FormDescription>
+                                                
+                                            </FormDescription>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="correo"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Correo electronico</FormLabel>
+                                            <FormControl>
+                                                <Input  placeholder="Escribe el correo electronico" {...field} type='email'/>
+                                            </FormControl>
+                                            <FormDescription>
+                                               
+                                            </FormDescription>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                            {loading && <Loader />}
+                        </div>
+                        <div className=" w-full flex justify-normal mt-4">
+                            <Button type="submit">Guardar</Button>
+                        </div>   
                     </form>
                 </Form>
             </div>
