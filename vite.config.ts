237fad1b3351163a4affdce0effc7dd744a87607
Cris <76029941+Kurisu-Cris@users.noreply.github.com>
2024-07-22 08:47:44 -0700

    impresora

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import mkcert from 'vite-plugin-mkcert'
  
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    //mkcert() // Añadir el plugin mkcert
+  ],
+
+  server: {
+    //https: true // Activar https
+  },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+
 })
