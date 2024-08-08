0023583035ae2a9a82c926037555a85190f7c2c9
Jeremy Ojeda Ceseña <hola2001.com@hotmail.com>
2024-08-08 12:49:57 -0700

    detalles

--- src/views/Layout/MenuSuperiosNew.tsx
+++ src/views/Layout/MenuSuperiosNew.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOMServer from 'react-dom/server';
 import {
     Menubar,
@@ -23,10 +23,13 @@ import { ContextProvider, useStateContext } from '../../contexts/ContextProvider
 import { MouseEvent } from 'react';
 import axiosClient from '../../axios-client';
 import {subMenuZustand} from "../../contexts/ZustandSubmenu.tsx"
+import { Skeleton } from "@/components/ui/skeleton"
+
 
 export const MenuSuperiosNew = () => {
-    const { setToken, setUser, user, permissions, setPermissions } = useStateContext();
+    const { setToken, setUser, user, permissions, setPermissions} = useStateContext();
     const {set_titulo, set_icono, titulo} = subMenuZustand();
+    const [loading_permissions, set_loading_permissions] = useState(false);
 
     const logout = (e: MouseEvent<SVGSVGElement>): void => {
         e.preventDefault();
@@ -48,15 +51,17 @@ export const MenuSuperiosNew = () => {
         getPermissions();
     }, [user]);
 
+
     //obtener los permisos del usuario
     const getPermissions = async () => {
+        set_loading_permissions(true);
         try {
             const response = await axiosClient.get(`/Rol/get_all_permissions_by_user_id/${user.id}`);
             setPermissions(response.data);
-            console.log(user.id)
-            console.log(response.data)
+            set_loading_permissions(false)
         } catch (error) {
             console.error("Failed to fetch anomalias:", error);
+            set_loading_permissions(false)
         }
     };
 
@@ -244,8 +249,19 @@ export const MenuSuperiosNew = () => {
         <>
             <p className='relative xl:hidden text-sm text-red-500 p-1 h-[9vh] flex items-center justify-center'>La resolucion no es compatible</p>
             <div className='relative hidden xl:block '>
+          
                 <Menubar>
-                    {opciones.map((opcionPadre, index) => {
+                    {
+                        loading_permissions &&
+                        <>
+                        <Skeleton className="ml-3 w-[70rem] h-[20px]" />
+                        </>
+                    }
+
+                    {
+                        !loading_permissions &&
+                        <>
+                           {opciones.map((opcionPadre, index) => {
                         if (permissions.includes(opcionPadre.permission) || user.id == 1) {
                             return (
                                 <MenubarMenu>
@@ -278,6 +294,9 @@ export const MenuSuperiosNew = () => {
                             )
                         }
                     })}
+                        </>
+                    }
+                 
 
                 </Menubar>
                 <div className=' h-full w-[200px] absolute right-5 flex items-center top-0  justify-center gap-3'>
