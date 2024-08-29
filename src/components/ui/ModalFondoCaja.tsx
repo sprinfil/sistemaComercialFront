342dbdd54a2146e8817fedb133d9ec9b569b48df
Cris <76029941+Kurisu-Cris@users.noreply.github.com>
2024-08-29 14:50:44 -0700

    Fondo, Retiro, Corte FINALIZADO

--- src/components/ui/ModalFondoCaja.tsx
+++ src/components/ui/ModalFondoCaja.tsx
@@ -58,6 +58,7 @@ export const ModalFondoCaja = ({ open, onRegister }) => {
             onBlur={handleBlur} // Formatea cuando el campo pierde el foco
             className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             placeholder="Ingrese la cantidad"
+            maxLength={5}
           />
           </div>
           <AlertDialogFooter>
