fd94eab09e3258af7837bbb3c491dc52eaa751e3
Cris <76029941+Kurisu-Cris@users.noreply.github.com>
2024-09-10 09:03:52 -0700

    cambios

--- src/views/Monitores/FactibilidadMonitor.tsx
+++ src/views/Monitores/FactibilidadMonitor.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import FactibilidadesTable from '../../components/Tables/FactibilidadesTable'
-import { FactibilidadProvider } from '../../contexts/FactibilidadContext'
 
 const FactibilidadMonitor = () => {
   return (
     <div>
-      <FactibilidadesTable/>
+      
     </div>
   )
 }
