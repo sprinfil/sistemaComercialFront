ff0e7277ae094e12e2eccb82fbba9ff626b01da3
Jeremy Ojeda Ceseña <112501128+sprinfil@users.noreply.github.com>
2024-09-17 14:47:16 -0700

    avance

--- src/views/Configuraciones/Catalogos/Convenios.tsx
+++ src/views/Configuraciones/Catalogos/Convenios.tsx
@@ -4,25 +4,26 @@ import ConvenioForm from '../../../components/Forms/ConvenioForm'
 import { ContextProvider } from "../../../contexts/ContextConvenio.tsx";
 import { useStateContext } from "../../../contexts/ContextConvenio.tsx";
 import { OcultarTable } from '../../../components/Tables/Components/OcultarTable.tsx';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 const Convenios = () => {
   return (
 
     <ContextProvider>
-    <div className='w-full max-h-[75vh] '>
+      <div className='w-full max-h-[75vh] '>
 
-      {/*Contenedor principal*/}
-      <div className='flex gap-2 '>
+        {/*Contenedor principal*/}
+        <div className='flex gap-2 '>
 
-        {/*Datatable*/}
+          {/*Datatable*/}
           <MostrarTable />
 
-        {/*Formulario*/}
+          {/*Formulario*/}
           <ConveniosFormEdit />
 
+        </div>
       </div>
-    </div>
-  </ContextProvider>
+    </ContextProvider>
   )
 }
 
@@ -35,30 +36,40 @@ const ConveniosFormEdit = () => {
 
   return (
     <>
-        {/*AQUI SE MANDA A LLAMAR EL FORMULARIO PERO CON LA VALIDACION SI ES EDITAR SE CAMBIE DE COLOR GG*/}
+      {/*AQUI SE MANDA A LLAMAR EL FORMULARIO PERO CON LA VALIDACION SI ES EDITAR SE CAMBIE DE COLOR GG*/}
       {accion == "editar" ? (<div className='w-full rounded-md border border-primary h-[77vh] p-4'>
-            <ConvenioForm />
-          </div>) : (<div className='w-full rounded-md border border-border h-[77vh] p-4 overflow-auto'>
-            <ConvenioForm />
-          </div>)}
+        <TabsConevios />
+      </div>) : (<div className='w-full rounded-md border border-border h-[77vh] p-4 overflow-auto'>
+        <TabsConevios />
+      </div>)}
     </>
   );
 };
 
 const MostrarTable = () => {
-
   const { accion } = useStateContext();
-
-  return(
+  return (
     <>
-        {/*Datatable*/}
-
+      {/*Datatable*/}
       <OcultarTable accion={accion}>
-      <ConvenioTable />
+        <ConvenioTable />
       </OcultarTable>
-      
     </>
   )
-
-
 };
+
+const TabsConevios = () => {
+  return (
+    <Tabs defaultValue="" className="">
+      <TabsList>
+        <TabsTrigger value="Convenio">Convenio</TabsTrigger>
+   
+      </TabsList>
+      <TabsContent value="Convenio">
+        <ConvenioForm />
+      </TabsContent>
+
+    </Tabs>
+  )
+
+}
